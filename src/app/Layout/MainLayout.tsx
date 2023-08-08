8e5e9c0c98d314164964561404b59a2f9bc81377
mohammadzeyghami <mohammadzeighami197@gmail.com>
2023-08-09 01:47:03 +0330

    start

---
+++ src/app/Layout/MainLayout.tsx
@@ -0,0 +1,44 @@
+// this is layout you can use this in those page you want
+import React from "react";
+import Stack from "@mui/material/Stack";
+
+interface Iprops {
+  children: React.ReactNode;
+}
+const MainLayout = ({ children }: Iprops) => {
+  return (
+    <Stack
+      mx={"auto"}
+      maxWidth={"1850px"}
+      mt={"20px"}
+      width={"100%"}
+      height={"95vh"}
+      flexDirection={"row"}
+    >
+      <Stack
+        width={"calc(25% - 20px)"}
+        mr={"20px"}
+        bgcolor={"red"}
+        height={"100%"}
+      >
+        SideBar
+      </Stack>
+      <Stack
+        flexDirection={"column"}
+        width={"calc(100% - 200px)"}
+        bgcolor={"gray"}
+        height={"100%"}
+      >
+        <Stack width={"100%"} bgcolor={"blue"} height={"50px"}>
+          Navbar
+        </Stack>
+        <Stack height={"calc(100% - 100px)"}>{children}</Stack>
+        <Stack width={"100%"} bgcolor={"blue"} height={"50px"}>
+          Footer
+        </Stack>
+      </Stack>
+    </Stack>
+  );
+};
+
+export default MainLayout;
---
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -0,0 +1,42 @@
+import type { NextAuthOptions } from "next-auth";
+import GitHubProvider from "next-auth/providers/github";
+import CredentialsProvider from "next-auth/providers/credentials";
+
+export const options: NextAuthOptions = {
+  providers: [
+    GitHubProvider({
+      clientId: process.env.GITHUB_ID as string,
+      clientSecret: process.env.GITHUB_SECRET as string,
+    }),
+    CredentialsProvider({
+      name: "Creadentials",
+      credentials: {
+        username: {
+          label: "UserName",
+          type: "text",
+          placeholder: "your-cool-username",
+        },
+        password: {
+          label: "Password:",
+          type: "password",
+          placeholder: "your-awesome-password",
+        },
+      },
+      async authorize(credentials) {
+        // This is where you need to retrieve user data
+        // to verify with credentials
+        // Docs: https://next-auth.js.org/configuration/providers/credentials
+        const user = { id: "24", name: "Mohammad", password: "nextauth" };
+
+        if (
+          credentials?.username === user.name &&
+          credentials?.password === user.password
+        ) {
+          return user;
+        } else {
+          return null;
+        }
+      },
+    }),
+  ],
+};
